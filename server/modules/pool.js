dda6de34cdb805dded2013a4592758def5c4c3d4
Adam Garberg <adamgarberg@gmail.com>
2022-02-11 06:28:51 -0600

    finished server.js

--- server/modules/pool.js
+++ server/modules/pool.js
@@ -1,27 +1,27 @@
 const pg = require('pg');
 
 // database pool options
-let poolOptions = {
-    database: 'koalas',
+const config = {
+    database: 'toDoDB',
     host: 'localhost',
     max: 10,
     // database on port 5432
     port: 5432,
     // timeout after 30 seconds of no connection
-    idleTimeoutMillis: 30_000,
+    idleTimeoutMillis: 30000,
 }
 
 // create a pool instance for the koala database
-const koalaPool = new pg.Pool(poolOptions);
+const pool = new pg.Pool(config);
 
 // give an indicator when connected to database
-koalaPool.on('connect', () => {
-    console.log('Connected to Koalas Database');
+pool.on('connect', () => {
+    console.log('Connected with To Do Database');
 })
 
 // handle database errors
-koalaPool.on('error', (err) => {
-    console.error('Error with Koalas Database:', err);
+pool.on('error', (err) => {
+    console.error('Error with TO DO Database:', err);
 })
 
-module.exports = koalaPool;
+module.exports = pool;
